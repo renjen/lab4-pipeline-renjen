4b99ebb99d886b234c5d62836de0b639a2ec9e24
renjen <renjende@gmail.com>
2025-04-29 15:57:36 -0700

    fixed the bug for lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,15 +6,14 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if (add) {
-        const result = 0;
-
-        result = num1 + num2;
-
-        return result;
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' || typeof add !== 'boolean') {
+        return false;
     }
-    else {
-        return !add;
+
+    if (add) {
+        return num1 + num2;
+    } else {
+        return false;
     }
 }
 
@@ -25,11 +24,17 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    const discounted = []
-    const length = prices.length;
-    let discountedPrice = 0
-    for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+    if (!Array.isArray(prices) || typeof discount !== 'number' || discount < 0 || discount > 1) {
+        return false;
+    }
+
+    if (prices.length === 0) {
+        return false;
+    }
+
+    const discounted = [];
+    for (let i = 0; i < prices.length; i++) {
+        let discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
